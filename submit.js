f806698b866cdb22401346641ec88ddd22e0e094
DESKTOP-BQDUELI\pc test <joshua.puglisi@gmail.com>
2021-10-29 19:46:31 +0200

    added all prototype files

---
+++ submit.js
@@ -0,0 +1,13 @@
+function download(){
+    var text = document.getElementById("my-text").value;
+    text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+    var blob = new Blob([text], { type: "odt"});
+    var anchor = document.createElement("a");
+    anchor.download = "story.odt";
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.target ="_blank";
+    anchor.style.display = "none"; // just to be safe!
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+ }
