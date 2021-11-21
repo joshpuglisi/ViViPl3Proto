e4468509293ef2525501b200087cfe76027e8702
DESKTOP-BQDUELI\pc test <joshua.puglisi@gmail.com>
2021-11-21 17:45:31 +0100

    added tab and changed input

--- example/submit.js
+++ example/submit.js
@@ -1,7 +1,9 @@
 function download(){
     var text = document.getElementById("my-text").value;
     text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
-    var blob = new Blob([text], { type: "odt"});
+    var text2 = document.getElementById("my-text2").value;
+    text2 = text2.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+    var blob = new Blob(["Damit " + text + ", moechte ich als " + text2], { type: "odt"});
     var anchor = document.createElement("a");
     anchor.download = "story.odt";
     anchor.href = window.URL.createObjectURL(blob);
